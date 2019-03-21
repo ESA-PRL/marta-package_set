f5ad2ba28ff17106465d5177880a292e9358c91f
Tim Wiese <tim.wiese@esa.int>
2019-03-21 13:39:06 +0100

    Added proper build for SOEM and removed EtherLab

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
-def has_etherlab?
-    file = File.join(ENV['HOME'],"rock","install","lib","pkgconfig","activemq-cpp.pc")
-    File.exists?(file)
-end
-def has_soem?
-    file = File.join(ENV['HOME'],"rock","install","lib","pkgconfig","activemq-cpp.pc")
-    File.exists?(file)
-end
-
+#def has_etherlab?
+#    file = File.join(ENV['HOME'],"rock","install","lib","pkgconfig","activemq-cpp.pc")
+#    File.exists?(file)
+#end
+#def has_soem?
+#    file = File.join(ENV['HOME'],"rock","install","lib","pkgconfig","activemq-cpp.pc")
+#    File.exists?(file)
+#end
+#
